8044e766a7e8d0c0fb333c8e16161a7ba8d24326
sqw@mvtech.cc <sqw123az@sina.com>
2020-06-16 17:47:33 +0800

    1、model新建完成 2、test数据导入数据库 3、xadmin集成

--- MallShop/MallShop/settings.py
+++ MallShop/MallShop/settings.py
@@ -11,9 +11,13 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import sys
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))  # 项目根目录
+sys.path.insert(0, BASE_DIR)
+sys.path.insert(0, os.path.join(BASE_DIR, 'apps'))
+sys.path.insert(0, os.path.join(BASE_DIR, 'extra_apps'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
@@ -26,6 +30,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+# 替换系统用户model
+AUTH_USER_MODEL = 'users.UserProfile'
 # Application definition
 
 INSTALLED_APPS = [
@@ -35,6 +41,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # 富文本
+    'DjangoUeditor',
+    'xadmin',
+    'crispy_forms',
+    'users',
+    'goods',
+    'trade',
+    'user_operation',
 ]
 
 MIDDLEWARE = [
@@ -102,9 +116,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'zh-hans'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Shanghai'
 
 USE_I18N = True
 
---
+++ MallShop/apps/users/models.py
@@ -0,0 +1,42 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.utils import timezone
+
+# Create your models here.
+
+class UserProfile(AbstractUser):
+    """
+    用户
+    """
+    # 新增字段
+    name = models.CharField(max_length=30, null=True, blank=True, verbose_name='姓名')
+    birthday = models.DateField(null=True, blank=True, verbose_name='出生年月')
+    gender = models.CharField(max_length=6, choices=(('male', u"男"), ('female', u"女")), default='female',
+                              verbose_name='性别')
+    mobile = models.CharField(max_length=11, verbose_name='电话')
+    email = models.CharField(max_length=100, null=True, blank=True, verbose_name='邮箱')
+
+    class Meta:
+        verbose_name = "用户"
+        verbose_name_plural = verbose_name
+
+    # 类的实例对象str化，用于print
+    def __str__(self):
+        return self.name
+
+
+class VerifyCode(models.Model):
+    """
+    短信验证码，后期变为redis存储
+    """
+    code = models.CharField(max_length=10, verbose_name='验证码')
+    mobile = models.CharField(max_length=11, verbose_name='电话')
+    add_time = models.DateTimeField(default=timezone.now, verbose_name='添加时间')  # 这里默认的时间为程序运行时间，不是添加记录的时间
+
+    class Meta:
+        verbose_name = "短信验证码"
+        verbose_name_plural = verbose_name
+
+    # 类的实例对象str化，用于print
+    def __str__(self):
+        return self.code
